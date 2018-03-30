bec9321f71a8c14651f824059ed64171d6f59a37
gavinmeier25 <gavinmeier41910@gmail.com>
2018-03-29 20:25:16 -0500

    Finished a couple test

--- Test/Inbox.test.js
+++ Test/Inbox.test.js
@@ -1,11 +1,13 @@
 const assert = require("assert");
 const ganache = require("ganache-cli");
 const Web3 = require("web3");
-const web3 = new Web3(ganache.provider());
+const provider = ganache.provider();
+const web3 = new Web3(provider);
 const { interface, bytecode } = require("../compile");
 
 let accounts;
 let inbox;
+const messageOfTruth= "Hi there";
 
 beforeEach(async () => {
   // get a list of all accounts
@@ -17,16 +19,24 @@ beforeEach(async () => {
   // use one of those accounts to deploy
   // the contract
   // returns a promise
- inbox = await new web3.eth.Contract(JSON.parse(interface))
+  inbox = await new web3.eth.Contract(JSON.parse(interface))
     .deploy({
       data: bytecode,
-      arguments: ['Hi there!']
+
+      arguments: [messageOfTruth]
     })
-    .send({ from: accounts[0], gas: "1000000" })
+    .send({ from: accounts[0], gas: "1000000" });
+
+  inbox.setProvider( provider );
 });
 
 describe("Inbox", () => {
   it("deploys a contract", () => {
-      console.log(inbox)
+    assert.ok(inbox.options.address);
+  });
+
+  it("has a default message", async () => {
+      const message = await inbox.methods.message().call();
+      assert.equal( message, messageOfTruth )
   });
 });
